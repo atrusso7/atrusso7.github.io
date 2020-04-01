a77e8680d98cac07d06759eed409c320c54d6b1f
Anthony Russo <atrusso7@gmail.com>
2020-04-01 15:18:04 -0500

    Update video.js

--- CheefulHour/static/js/video.js
+++ CheefulHour/static/js/video.js
@@ -1,13 +1,13 @@
 // Determine the room name and public URL for this chat session.
 function getRoom() {
-    var query = location.search && location.search.split("?")[1];
+  var query = location.search && location.search.split("?")[1];
 
-    if (query) {
-      return (location.search && decodeURIComponent(query.split("=")[1]));
-    }
-
-    return okta.tokenManager.get("idToken").claims.email;
+  if (query) {
+    return (location.search && decodeURIComponent(query.split("=")[1]));
   }
+
+  return okta.tokenManager.get("idToken").claims.email;
+}
 
 // Retrieve the absolute room URL.
 function getRoomURL() {
